ea78030ee0cf783b2ed3c6e2a4b17feeca782808
shreebhagwanrbpl <rajbiosis12@gmail.com>
2026-05-11 10:13:47 +0530

    changes in multiple code files

--- app/websites/humanbiomedicalorg/district/page.js
+++ app/websites/humanbiomedicalorg/district/page.js
@@ -10,7 +10,7 @@ import {
 } from "firebase/firestore";
 
 export default function Page() {
-    const WEBSITE = "indiandiagnostic"; 
+    const WEBSITE = "humanbiomedicalorg"; 
   const [jsonData, setJsonData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [fileName, setFileName] = useState("");
